73346c106ce41801f61f09fd947600bd8db27357
wpoplaws <wojciechpoplawski94@gmail.com>
2020-04-07 14:15:33 +0200

    add template tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+from ..models import Post
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/latest_posts.html')
+def show_latest_posts(count=3):
+    latest_posts = Post.published.order_by('-date_added')[:count]
+    return {'latest_posts': latest_posts}
