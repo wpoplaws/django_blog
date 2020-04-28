5439223cfbcda039d1e99fc1f8cddbe6968d777c
Wojciech Popławski <wojciechpoplawski94@gmail.com>
2020-04-28 12:05:34 +0200

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -101,10 +101,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
